387a2083d53ad117272ffa83f22710abdd0a3e85
mahdinazariii <Mahdinazari2251@gmail.com>
2025-05-15 00:11:48 +0330

    part 2

--- part2/app/ShowPanel.java
+++ part2/app/ShowPanel.java
@@ -1,8 +1,49 @@
 package part2.app;
 
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class ShowPanel extends JPanel {
+    private Image image ;
+    private String imagePath = "images/img.png" ;
+    public ShowPanel() {
+
+        setLayout(new BorderLayout());
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        loadImage();
+
+
+    }
+
+    public void loadImage() {
+
+        try {
+            image = ImageIO.read(getClass().getResource(imagePath));
+
+        }catch(IOException e) {
+            e.printStackTrace();
+
+        }
+        repaint();
+
+    }
+    public void scaleImage(int width, int height) {
+        if(image != null) {
+            Image scaled = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            image = scaled;
+            repaint();
+        }
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if(image != null ){
+            int x = (getWidth() - image.getWidth(null)) / 2;
+            int y = (getHeight() - image.getHeight(null)) / 2;
+            g.drawImage(image,x,y,this);
+        }
+    }
     
 }
 
