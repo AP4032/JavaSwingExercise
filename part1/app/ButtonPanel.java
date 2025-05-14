0a164152589131effa3735b2dfdb0e6106759007
LiGER <samarghandi112w34@gmail.com>
2025-05-14 14:05:29 +0330

    Calculators buttons are in place

--- part1/app/ButtonPanel.java
+++ part1/app/ButtonPanel.java
@@ -1,20 +1,16 @@
 package part1.app;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ButtonPanel extends JPanel{
-    //One to Nine number btns components
-    private JButton btnNum1; 
-    private JButton btnNum2; 
-    private JButton btnNum3; 
-    private JButton btnNum4; 
-    private JButton btnNum5; 
-    private JButton btnNum6; 
-    private JButton btnNum7; 
-    private JButton btnNum8; 
-    private JButton btnNum9; 
-    private JButton btnNum0; 
+    private GridBagConstraints gbc;
+
+    private JButton[] numberButtons;
 
     //Operation buttons , * / - + = 
     private JButton equalBtn;
@@ -23,12 +19,74 @@ public class ButtonPanel extends JPanel{
     private JButton divBtn;
     private JButton multiplayBtn;
 
-    private JButton btnC;
+    private JButton clearBtn;
     //the P stands for parentheses
     private JButton openP;
     private JButton closeP;
+    private JButton backBtn;
 
     private JButton decimalPointBtn;
 
-    
+    public ButtonPanel(){
+        numberButtons = new JButton[10];
+        //initialize the numbers btns.
+        for (int i =0; i<=9;i++){
+           numberButtons[i] = new JButton(String.valueOf(i)); 
+        }
+
+        equalBtn = new JButton("=");
+        minusBtn = new JButton("-");
+        plusBtn = new JButton("+");
+        divBtn = new JButton("/");
+        multiplayBtn = new JButton("*");
+
+        clearBtn = new JButton("C");
+        backBtn = new JButton("b");
+        openP = new JButton("("); 
+        closeP = new JButton(")"); 
+        decimalPointBtn = new JButton(".");
+
+        this.setLayout(new GridBagLayout());
+
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        //First row
+        placeBtn(0,0, numberButtons[7]);
+        placeBtn(0,1, numberButtons[8]);
+        placeBtn(0,2, numberButtons[9]);
+        placeBtn(0,3, divBtn);
+
+        //Second row
+        placeBtn(1,0, numberButtons[4]);
+        placeBtn(1,1, numberButtons[5]);
+        placeBtn(1,2, numberButtons[6]);
+        placeBtn(1,3, multiplayBtn);
+
+        //Thirth row
+        placeBtn(2,0, numberButtons[1]);
+        placeBtn(2,1, numberButtons[2]);
+        placeBtn(2,2, numberButtons[3]);
+        placeBtn(2,3, minusBtn);
+
+        //Fourth row
+        placeBtn(3,0, numberButtons[0]);
+        placeBtn(3,1, decimalPointBtn);
+        placeBtn(3,2,equalBtn); 
+        placeBtn(3,3, plusBtn);
+
+        //Fifth row
+        placeBtn(4,0, clearBtn);
+        placeBtn(4,1, openP);
+        placeBtn(4,2, closeP); 
+        placeBtn(4,3, backBtn);
+    }
+
+    //helper method for adding btns 
+    public void placeBtn (int x, int y, JButton btn){
+        gbc.gridx = x;
+        gbc.gridy = y;
+        add(btn, gbc);
+    }
 }
