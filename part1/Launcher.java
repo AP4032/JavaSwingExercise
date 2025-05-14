0e719fb77bf6049942a71c64537dcff3074ae0ea
mahdinazariii <Mahdinazari2251@gmail.com>
2025-05-14 21:24:02 +0330

    part 1

--- part1/Launcher.java
+++ part1/Launcher.java
@@ -6,9 +6,8 @@ import part1.app.CalculatorApp;
 
 public class Launcher {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-           new CalculatorApp(); 
-        });
+        new CalculatorApp().setVisible(true);
+
     }
     
 }
--- part1/app/DisplayPanel.java
+++ part1/app/DisplayPanel.java
@@ -1,7 +1,28 @@
 package part1.app;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class DisplayPanel extends JPanel{
+
+    private JTextField displayField;
+
+    public DisplayPanel(){
+
+        displayField = new JTextField();
+        this.setPreferredSize(new Dimension(280,70));
+        this.setBorder(BorderFactory.createEmptyBorder(5,10,5,10));
+        this.setFont(new Font("Arial",Font.PLAIN,24));
+        displayField.setHorizontalAlignment(SwingConstants.LEFT);
+        this.setLayout(new BorderLayout());
+        this.add(displayField, BorderLayout.CENTER);
+
+    }
+    public String getDisplay(){
+        return displayField.getText();
+    }
+    public void setDisplay(String display){
+        this.displayField.setText(display);
+    }
     
 }
