a6f40a37433832695424d74063a31c27b54f1776
ElaheNejadasghar <elahenejadasghar@gmail.com>
2025-05-17 13:38:07 +0330

    part 3 finished.

--- part3/Launcher.java
+++ part3/Launcher.java
@@ -5,9 +5,9 @@ import javax.swing.SwingUtilities;
 import part3.app.components.MainFrame;
 
 public class Launcher {
-     public static void main(String[] args){
-    SwingUtilities.invokeLater(() -> {
-           new MainFrame(); 
-    });
- }
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() -> {
+            new MainFrame();
+        });
+    }
 }
