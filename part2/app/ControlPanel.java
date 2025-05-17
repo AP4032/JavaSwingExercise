c76c3945426ca1656f528178f142fb3e535ae959
Ahmadzadhe <gokerxxbg@gmail.com>
2025-05-18 03:10:54 +0330

    part2End

--- part2/app/ControlPanel.java
+++ part2/app/ControlPanel.java
@@ -1,7 +1,50 @@
 package part2.app;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
 
 public class ControlPanel extends JPanel {
-    
+    private JButton button;
+    private JTextField textFieldX;
+    private JTextField textFieldY;
+    private GridBagConstraints gbc;
+    ControlPanel (ShowPanel showPanel, int Max, int May){
+        ActionListener DetailsButton = e -> {
+
+            try {
+                int X = Integer.parseInt(textFieldX.getText());
+                int Y = Integer.parseInt(textFieldY.getText());
+                if(X>=Max || Y>=May){
+                    JOptionPane.showMessageDialog(null,"Entered size cannot be larger than the panel size!");
+                }else {
+                    showPanel.setXAndY(X,Y);
+
+                }
+            }catch (NumberFormatException ex){
+                JOptionPane.showMessageDialog(null, "Please enter valid numbers for width and height");
+            }
+        };
+        button=new JButton("Scale Image");
+        button.addActionListener(DetailsButton);
+        textFieldX=new JTextField(5);
+        textFieldY=new JTextField(5);
+        gbc=new GridBagConstraints();
+        gbc.insets=new Insets(10,10,10,10);
+        gbc.fill =GridBagConstraints.HORIZONTAL;
+        gbc.gridx=0; gbc.gridy=0;
+        add(new JLabel("width: "));
+        gbc.gridx=1;
+        add(textFieldX,gbc);
+        gbc.gridx=2;
+        add(new JLabel("height: "),gbc);
+        gbc.gridx=3;
+        add(textFieldY,gbc);
+        gbc.gridx=4;
+        add(button,gbc);
+
+
+    }
+
 }
