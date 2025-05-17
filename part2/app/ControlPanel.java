ceb478e65f0ef583f8a8dd22fad5670a95f2a50e
Faeze-Ghandi <faezeghandi91@gmail.com>
2025-05-18 00:02:07 +0330

    done

--- part2/app/ControlPanel.java
+++ part2/app/ControlPanel.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class ControlPanel extends JPanel {
     private JTextField widthField, heightField;
     private JButton scaleButton;
