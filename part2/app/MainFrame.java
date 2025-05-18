66ff7da9e41f74375569c7eb93fde7acf457f784
ITech <elahehvaseqi@gmail.com>
2025-05-18 14:35:31 +0330

    done

--- part2/app/MainFrame.java
+++ part2/app/MainFrame.java
@@ -1,8 +1,26 @@
 package part2.app;
 
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
-    
-}
 
+    private ShowPanel showPanel;
+    private ControlPanel controlPanel;
+
+    public MainFrame() {
+        setTitle("Image Scale");
+        setSize(600, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        showPanel = new ShowPanel();
+        controlPanel = new ControlPanel(showPanel);
+
+        add(showPanel, BorderLayout.CENTER);
+        add(controlPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+}
