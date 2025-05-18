b1f3a5bd1b1ddffa9e2f50104146577efd040953
Sarina <sarina.shakeri0806@gmail.com>
2025-05-18 22:57:18 +0330

    JSwing is completed.

--- part2/Launcher.java
+++ part2/Launcher.java
@@ -1,13 +1,13 @@
 package part2;
 
 import javax.swing.SwingUtilities;
-
 import part2.app.MainFrame;
 
 public class Launcher {
     public static void main(String[] args) {
+        // Launch the MainFrame on the Event Dispatch Thread
         SwingUtilities.invokeLater(() -> {
-           new MainFrame(); 
+            new MainFrame(); // Create and show the main frame
         });
     }
 }
